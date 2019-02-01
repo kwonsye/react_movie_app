559535ae37243ad730b5242498dcce703fcfb039
kwonsye <kwonsye@naver.com>
2019-02-02 03:11:13 +0900

    Updates

---
+++ precache-manifest.d0e58a9f7deb6a24cede894d42f72c5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb59124bc5ccf430e819",
+    "url": "/react_movie_app/static/js/runtime~main.cb59124b.js"
+  },
+  {
+    "revision": "e0415fe94442d473b6a7",
+    "url": "/react_movie_app/static/js/main.e0415fe9.chunk.js"
+  },
+  {
+    "revision": "1bf244e142401c5b4106",
+    "url": "/react_movie_app/static/js/1.1bf244e1.chunk.js"
+  },
+  {
+    "revision": "e0415fe94442d473b6a7",
+    "url": "/react_movie_app/static/css/main.37763736.chunk.css"
+  },
+  {
+    "revision": "20ac85806fc70267715c11895cb04789",
+    "url": "/react_movie_app/index.html"
+  }
+];
